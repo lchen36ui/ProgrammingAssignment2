16a8c5aa36de885b98ee0c6477db0c91663b36f8
Alex <lchen36@illinois.edu>
2015-01-25 14:34:23 -0600

    Updated cacheMatrix.R
    As required by Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse ##
 
-## Write a short comment describing this function
+	makeCacheMatrix <- function(x = matrix()) {
+		## defines where to store inverse null ##
+    	inverse <- NULL
+		
+		## matrix y as input to the variable x##
+    		## it is used to alter the matrix ##
+    	set <- function(y) {
+        x <<- y
+		## initialize inverse in the parent environment to null ##
+        inverse <<- NULL 
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
+    		## returns the raw matrix ##
+    	get <- function() x
+
+    		## setinverse sets the inverse variable ##
+    	setinverse <- function(i) 
+        inverse <<- i
+    
+
+    		## getinverse gets the cached inverse ##
+    getinverse <- function() 
+        inverse
+    
+
+   	 	## return the special matrix ##
+   	 list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)    
+
 
 }
 
 
-## Write a short comment describing this function
+## The second function "cacheSolve" calculates the inverse of what returned by the above makeCacheMatrix. ##
+## cacheSolve will use the inverse directly from the cache if it was calculated. ##
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x' ##
+
+	 inverse <- x$getinverse()
+
+	    	if(!is.null(inverse)) {
+        
+        message("getting inverse from the cache")
+        ## return the already-cached inverse ##
+		
+	return(inverse)
+    }
+
+    ## if the inverse has not been cached, do the following ## 
+    	
+	 	mtrx <- x$get()
+    	 	inverse <- solve(mtrx, ...)
+    	 	x$setinverse(inverse)
+    ## then compute the inverse and cache it ##
+
+	return(inverse)
+
 }
